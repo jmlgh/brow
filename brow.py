edf5435fb498bbe2524d5cb2abff1a7fb1341363
jfml <jfmarli@gmail.com>
2024-10-06 21:33:51 +0200

    supporting https

--- brow.py
+++ brow.py
@@ -1,13 +1,21 @@
 import socket
+import ssl
 
 
 class URL:
     def __init__(self, url):
-        # http://example.org:80
+        # http://example.org
         self.scheme, url = url.split("://", 1)
-        assert self.scheme == "http"
+        assert self.scheme in ["http", "https"]
+
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+
         if "/" not in url:
             url = url + "/"
+
         self.host, url = url.split("/", 1)
         self.path = "/" + url
 
@@ -17,15 +25,22 @@ class URL:
             type=socket.SOCK_STREAM,
             proto=socket.IPPROTO_TCP,
         )
-        s.connect((self.host, 80))
+        s.connect((self.host, self.port))
+
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
+
         request = "GET {} HTTP/1.0\r\n".format(self.path)
         request += "Host: {}\r\n".format(self.host)
         request += "\r\n"
         s.send(request.encode("utf8"))
+
         response = s.makefile("r", encoding="utf-8", newline="\r\n")
         # status line
         statusline = response.readline()
         version, status, explanation = statusline.split(" ", 2)
+
         # headers
         response_headers = {}
         while True:
@@ -34,8 +49,10 @@ class URL:
                 break
             header, value = line.split(":", 1)
             response_headers[header.casefold()] = value.strip()
+
         assert "transfer-encoding" not in response_headers
         assert "content-encoding" not in response_headers
+
         # content
         content = response.read()
         s.close()
